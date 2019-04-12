6488bc5e5b224fc0bd39b1e11605be3e35c28e29
Harry Blum <himion0@gmail.com>
2019-04-12 12:26:43 +0100

    Init

---
+++ index.js
@@ -0,0 +1,44 @@
+const route53 = new require('aws-sdk').Route53();
+
+function respond(cb, statusCode, body) {
+  cb({
+    statusCode,
+    body: body && JSON.stringify(body)
+  });
+}
+
+async function handler(event, context, cb) {
+  const { ip, domain, secret } = JSON.parse(event.body);
+
+  if (!ip || !domain) return respond(cb, 400);
+
+  if (secret !== process.env.SECRET) return respond(cb, 403);
+
+  const params = {
+    ChangeBatch: {
+      Changes: [
+        {
+          Action: 'UPSERT',
+          ResourceRecordSet: {
+            Name: domain,
+            ResourceRecords: [
+              {
+                Value: ip
+              }
+            ],
+            TTL: 300,
+            Type: 'A'
+          }
+        }
+      ],
+      Comment: 'Web server for example.com'
+    },
+    HostedZoneId: process.env.HostedZoneId,
+  };
+
+  const result = await route53.changeResourceRecordSets(params).promise();
+
+  respond(cb, 200, result);
+}
+
+module.exports.handler = handler;
